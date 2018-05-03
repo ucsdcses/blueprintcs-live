8d4298bc264ac8b64a58da6ed0806954e0b5c1a8
Jacob Davis <jsdavis1010@gmail.com>
2018-05-02 20:33:32 -0700

    Made webpack env dependent

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: process.env.NODE_ENV || 'production',
     entry: './src/assets/js/main.js',
     output: {
         filename: 'main.js',
